f50ba06e43210fa1ef477cff987c87f6c996215a
EladAM52 <93151656+EladAM52@users.noreply.github.com>
2023-08-02 21:06:20 +0300

    Add files via upload

---
+++ TA/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TravelAgencyProject
+{
+    public class RouteConfig
+    {
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "SingUp",
+               url: "Login/NewUser",
+               defaults: new { controller = "Login", action = "SingUp", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+              name: "Success",
+              url: "Flights/Success",
+              defaults: new { controller = "Flights", action = "Booking", id = UrlParameter.Optional }
+          );
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional, extraparm = UrlParameter.Optional }
+            );
+        }
+    }
+}
